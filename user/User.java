0a8c0a97b3f2d22d377e3c75f0c1e039d50925f0
juliahusainzada <juliahusainzada@gmail.com>
2025-10-10 20:32:12 -0700

    Move User class to user package

---
+++ user/User.java
@@ -0,0 +1,19 @@
+package user;
+
+abstract class User {
+    protected String userId;
+    protected String displayName;
+
+    public User(String userId, String displayName) {
+        this.userId = userId;
+        this.displayName = displayName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
