b388fa9a300bc02e0090eece054076c3efe73113
juliahusainzada <juliahusainzada@gmail.com>
2025-10-10 20:32:19 -0700

    Add Artist class with song publishing capabilities

---
+++ user/Artist.java
@@ -0,0 +1,67 @@
+package user;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import domain.Song;
+
+import catalog.Catalog;
+
+public class Artist extends User {
+    private String stageName;
+    private Set<String> ownedItemIds;
+
+    /**
+     * userId = artist uniqueID for User
+     * stageName
+     * ownedItemIds = IDs of media artist owns (songs published)
+     */
+    public Artist(String userId, String displayName, String stageName) {
+        super(userId, displayName);
+        this.stageName = stageName;
+        this.ownedItemIds = new HashSet<>();
+
+        // auto-register artist in catalog
+        Catalog.getInstance().addArtist(this);
+    }
+
+    public String getStageName() {
+        return stageName;
+    }
+
+    public void setStageName(String stageName) {
+        this.stageName = stageName;
+    }
+
+    public Set<String> getOwnedItemIds() {
+        return ownedItemIds;
+    }
+
+    /**
+     * artist creates song object
+     * this.getUserId() - link for ownerArtistId
+     * add to catalog, which stores it in itemsById
+     */
+    public void publishSong(Catalog catalog, String title, int durationSec, String album, String genre) {
+        String itemId = "song_" + UUID.randomUUID().toString(); // simple ID generation
+        Song song = new Song(itemId, title, durationSec, this.getUserId(), album, genre);
+        catalog.addItem(song);
+        ownedItemIds.add(itemId);
+    }
+
+    /**
+    public void publishPodcast(Library library, String title, int durationSec, String seriesName, int episodeNumber) {
+        String itemId = "podcast_" + System.currentTimeMillis();
+        Podcast podcast = new Podcast(itemId, title, durationSec, personId, seriesName, episodeNumber);
+        library.addPodcast(this, podcast);
+    }
+    **/
+
+    public void removeOwnedItem(Catalog catalog, String itemId) {
+        if (ownedItemIds.contains(itemId)) {
+            catalog.removeItem(itemId);
+            ownedItemIds.remove(itemId);
+        }
+    }
+}
